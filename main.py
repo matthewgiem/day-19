392d46ff7281cd8048c33fd24daedb17225633a4
Matthew Giem <matthew.giem.programmer@gmail.com>
2023-05-17 16:19:08 +0700

    create turtle etch a sketch

---
+++ main.py
@@ -0,0 +1,37 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+
+def move_forwards():
+    tim.forward(10)
+
+
+def move_backwards():
+    tim.backward(10)
+
+
+def turn_counter_clockwise():
+    tim.left(5)
+
+
+def turn_clockwise():
+    tim.right(5)
+
+
+def clear_drawing():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
+
+
+screen.listen()
+screen.onkey(key="w", fun=move_forwards)
+screen.onkey(key="s", fun=move_backwards)
+screen.onkey(key="a", fun=turn_counter_clockwise)
+screen.onkey(key="d", fun=turn_clockwise)
+screen.onkey(key="c", fun=clear_drawing)
+
+screen.exitonclick()
